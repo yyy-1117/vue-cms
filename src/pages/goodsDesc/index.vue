ab96d551c4b61f29dc5fea839328aa88c5e9c32d
萌城 <122312825@qq.com>
2019-02-16 21:20:12 +0800

    完成图文介绍页面

--- src/pages/goodsDesc/index.vue
+++ src/pages/goodsDesc/index.vue
@@ -1,5 +1,49 @@
 <template>
-    <div>
-        商品详情
+    <div class="goodsDesc-container">
+        <h1 class="title">{{goodsDesc.title}}</h1>
+        <hr>
+        <div v-html="goodsDesc.content"></div>
     </div>
 </template>
+
+<script>
+import {Toast} from 'mint-ui'
+
+export default {
+    data(){
+        return{
+            id:this.$route.params.id,
+            goodsDesc:{}
+        }
+    },
+    methods:{
+        getgoodsDesc(){
+            this.$http.get('goods/getdesc/'+ this.id).then( result => {
+                console.log(result)
+                if(result.body.status === 0){
+                    this.goodsDesc = result.body.message[0]
+                }else{
+                    Toast('获取图文介绍失败')
+                }
+            })
+        }
+    },
+    created(){
+        this.getgoodsDesc()
+    }
+}
+</script>
+
+<style lang="less">
+    .goodsDesc-container{
+        .title{
+            color: #226aff;
+            font-size: 16px;
+            text-align: center;
+            padding: 10px 0;
+        }
+        img{
+            width: 100%;
+        }
+    }
+</style>
